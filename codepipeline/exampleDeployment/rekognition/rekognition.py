b85ac1a76c0eab05428cd20d0b9e4d6febf7770f
Llewellyn <cjl@8c859041024e.ant.amazon.com>
2018-05-01 17:45:02 +0100

    Test

---
+++ codepipeline/exampleDeployment/rekognition/rekognition.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+import os
+
+s3 = boto3.client('s3')
+rekognition = boto3.client('rekognition')
+
+
+def runRekognition(event):
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': event[1]['Records'][0]['s3']['bucket']['name'],
+                'Name': event[1]['Records'][0]['s3']['object']['key'],
+            }
+        },
+        MaxLabels=50,
+    )
+    return response
+
+def lambda_handler(event, context):
+    print(event[1]['Records'][0]['s3']['bucket']['name'])
+    print(event[1]['Records'][0]['s3']['object']['key'])
+    print(runRekognition(event))
+    return 
