cfa748bf1148f2525ce6ce2f01d86cf1effa5da3
Llewellyn <cjl@8c859041024e.ant.amazon.com>
2018-07-03 05:38:13 +0100

    Added code for transcription

---
+++ transcribe/transcribe.py
@@ -0,0 +1,12 @@
+#from lxml import etree
+#from docx import Document
+#from docx.shared import Cm, Mm, Inches, RGBColor
+#from docx.enum.text import WD_ALIGN_PARAGRAPH
+#import json, datetime, sys, boto3
+#import matplotlib.pyplot as plt
+#import statistics
+
+def lambda_handler(event, context):
+    print(event)
+    # TODO implement
+    return 'Complete'
