39ba409f81fa4a643037996b35de946146baf093
Llewellyn <cjl@8c859041024e.ant.amazon.com>
2019-05-03 15:00:19 +0100

    add object id not etag

--- codepipeline/exampleDeployment/writeOutput/writeOutput.py
+++ codepipeline/exampleDeployment/writeOutput/writeOutput.py
@@ -6,7 +6,7 @@ s3 = boto3.client('s3')
 
 def writeObject(event):
     bucket = os.environ['OUTPUT_BUCKET']
-    key = event[0]['ProcessingEngine'] + '/' + event[1]['Records'][0]['s3']['object']['eTag']
+    key = event[0]['ProcessingEngine'] + '/' + event[1]['Records'][0]['s3']['object']['key']
     data = event[2]
     data['ObjectId'] = key
     fileData = str.encode(json.dumps(data))
